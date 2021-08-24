65f15ca50e2b4e5adb3c464f7763a7c5a0ea638f
ny0011 <nayun011@gmail.com>
2021-08-24 11:00:29 +0900

    웹 찍먹반 - 1주차 : CRA로 리액트 프로젝트 구성하기

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ~ 김교수님 강의 ~
         </p>
         <a
           className="App-link"
