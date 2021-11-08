aab457937429082d622ecf935db2bef7fc2fbbd3
ny0011 <nayun011@gmail.com>
2021-11-08 19:42:51 +0900

    feat: firebase, react-router 추가
    firebase
    - config 추가 : fbase.js
    - npm 설치
    
    react-router
    - router 설정
    - navigation 생성
    - 기존 App.js 코드 -> Home.js 로 이동

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import { createGlobalStyle } from "styled-components";
-import "./App.css";
-import TodoHead from "./components/TodoHead";
-import TodoList from "./components/TodoList";
-import { TodoTemplate } from "./components/TodoTemplate";
-import TodoListContext from "./context/TodoContext";
-import TodoCreate from "./components/TodoCreate";
-import { useReducer, useRef } from "react";
-import reducer, { initialState } from "./context/TodoListReducer";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #e9e9e9;
-  }
-`;
+import React from "react";
+import AppRouter from "components/Router";
 
 function App() {
-  //const mockdata = [{ id: 0, text: "놀기", done: true }];
-  const defaultData =
-    JSON.parse(sessionStorage.getItem("data")) || initialState;
-  const [data, dispatch] = useReducer(reducer, defaultData);
-  let nextId = useRef(1);
-
   return (
     <>
-      <GlobalStyle></GlobalStyle>
-      <TodoTemplate>
-        <TodoListContext.Provider value={{ dispatch, data, nextId }}>
-          <TodoHead></TodoHead>
-          <TodoList></TodoList>
-          <TodoCreate></TodoCreate>
-        </TodoListContext.Provider>
-      </TodoTemplate>
+      <AppRouter />
     </>
   );
 }
--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -11,7 +11,7 @@ const TodoTemplateBlock = styled.div`
 
   margin: 0 auto;
 
-  margin-top: 96px;
+  margin-top: 64px;
   margin-bottom: 32px;
   display: flex;
   flex-direction: column;
